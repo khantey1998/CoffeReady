64f9a2b6418d9954adde22ccb9d759eb14e47f4b
khantey1998 <khanteylong@gmail.com>
2018-12-05 07:51:25 +0700

    Initial commit

---
+++ config/services.php
@@ -0,0 +1,50 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Stripe, Mailgun, SparkPost and others. This file provides a sane
+    | default location for this type of information, allowing packages
+    | to have a conventional place to find your various credentials.
+    |
+    */
+
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+    ],
+
+    'ses' => [
+        'key' => env('SES_KEY'),
+        'secret' => env('SES_SECRET'),
+        'region' => env('SES_REGION', 'us-east-1'),
+    ],
+
+    'sparkpost' => [
+        'secret' => env('SPARKPOST_SECRET'),
+    ],
+
+    'stripe' => [
+        'model' => App\User::class,
+        'key' => env('STRIPE_KEY'),
+        'secret' => env('STRIPE_SECRET'),
+        'webhook' => [
+            'secret' => env('STRIPE_WEBHOOK_SECRET'),
+            'tolerance' => env('STRIPE_WEBHOOK_TOLERANCE', 300),
+        ],
+    ],
+    // 'firebase' => [
+    // 'api_key' => 'AIzaSyCRjYmd1LneCczqVQaqLwdFbDJ1Tk60FVE', // Only used for JS integration
+    // 'auth_domain' => 'auth_domain', // Only used for JS integration
+    // 'database_url' => 'https://database_url.com/',
+    // 'secret' => 'secret',
+    // 'storage_bucket' => '', // Only used for JS integration
+    //     ],
+
+];
